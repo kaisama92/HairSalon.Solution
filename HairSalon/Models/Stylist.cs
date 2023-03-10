e14ca28ddda9598d00fc03f1e3e04b49bfc00b26
Kai Clausen <kaiclausen123@gmail.com>
2023-03-10 10:59:29 -0800

    add pronouns and date fields

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -7,6 +7,8 @@ namespace HairSalon.Models
     public int StylistId { get; set; }
     public string Name { get; set; }
     public string Specialty { get; set; }
+    public string Pronouns { get; set; }
+    public DateTime Date { get; set; }
     public List<Client> Clients { get; set; }
   }
 }
