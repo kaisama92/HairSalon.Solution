52e43291727588a13ab05ac56c98e580d10805ee
Kai Clausen <kaiclausen123@gmail.com>
2023-03-10 10:14:14 -0800

    add HomeController and start ClientController

---
+++ HairSalon/Controllers/ClientController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Collections.Generic;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class ClientController : Controller 
+  {
+
+  }
+}
